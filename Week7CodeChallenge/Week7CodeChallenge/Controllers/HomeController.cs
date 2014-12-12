0fc49ef7807f5878a92879f60ccab0f5ca0b1d58
Eric Fowler <ericfowler303@gmail.com>
2014-12-12 16:06:27 -0700

    Fill out the HomeController

--- Week7CodeChallenge/Week7CodeChallenge/Controllers/HomeController.cs
+++ Week7CodeChallenge/Week7CodeChallenge/Controllers/HomeController.cs
@@ -9,7 +9,29 @@ namespace Week7CodeChallenge.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: About
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: Work
+        [HttpGet]
+        public ActionResult Work()
+        {
+            return View();
+        }
+
+        // GET: Careers
+        [HttpGet]
+        public ActionResult Careers()
         {
             return View();
         }
---
+++ Week7CodeChallenge/Week7CodeChallenge/Models/ContactFormVerify.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7CodeChallenge.Models
+{
+    class ContactFormVerify
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNum { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string ProjectDescription { get; set; }
+        [Required]
+        public string Comment { get; set; }
+    }
+}

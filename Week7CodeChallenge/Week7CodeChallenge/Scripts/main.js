c44400e4d555f5fd8c8e5ed9dcfadcea45c0e1c8
Eric Fowler <ericfowler303@gmail.com>
2014-12-12 16:59:27 -0700

    Fix up some layouts for the ajax navigation

--- Week7CodeChallenge/Week7CodeChallenge/Scripts/main.js
+++ Week7CodeChallenge/Week7CodeChallenge/Scripts/main.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(document).ready(function() {
+    // create a selector to select the ajax-links for navigation between get/post actions
+    $("header, footer").on("click", ".ajax-links", function() {
+        // Get the url for the word that the user clicked on 
+        var url = $(this).data("url");
+
+        // Do a get request for that url
+        $.get(url, function(urlContent) {
+            // get the main div an replace the html data there
+            $("#mainBody").html(urlContent);
+        });
+    });
+});
